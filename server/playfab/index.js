f5c612e77cc123c7eb10b03a20664c37e8596d48
Yonas Berhe <sanoy@Yonass-MacBook-Air.local>
2015-10-10 15:09:48 -0700

    Over to this repo

--- server/playfab/index.js
+++
@@ -1,7 +0,0 @@
-import Config from './locals'
-import Playfab from './playfabAPI'
-
-Playfab.settings.title_id = Config.playfab.title_id
-Playfab.settings.developer_secret_key = Config.playfab.developer_secret_key
-
-export default Playfab
