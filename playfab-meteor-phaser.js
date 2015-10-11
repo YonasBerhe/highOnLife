fb9f6b72661fcae951afca1263fe5644fad1f95e
Yonas Berhe <sanoy@Yonass-MacBook-Air.local>
2015-10-10 21:23:54 -0700

    login with play flab and routing

--- playfab-meteor-phaser.js
+++ playfab-meteor-phaser.js
@@ -1,69 +1,47 @@
-// if (Meteor.isClient) {
-  // Template.game.helpers(
-  //
-  //
-  // );
-
-
-// }
-
 if (Meteor.isClient) {
 
-
-
-  var LocalStorage = {
-      getItem: function getItem(item){
-          return JSON.parse(localStorage.getItem(item))
-      },
-      setItem : function setItem(itemName, itemValue){
-          return localStorage.setItem(itemName, JSON.stringify(itemValue))
-      },
-      clear: function clear(itemName){
-          return localStorage.clear(itemName)
-      }
-  }
-
-
-
-  function isLoggedIn(){
-      return LocalStorage.getItem('User') ? true : false
-  }
-
-
-  Router.route('/', function () {
-    var is =  true;
-     console.log(isLoggedIn());
-    if(isLoggedIn()) {
-      this.render('game')
-    } else {
-      this.render('login');
+    var LocalStorage = {
+        getItem: function getItem(item) {
+            return JSON.parse(localStorage.getItem(item))
+        },
+        setItem: function setItem(itemName, itemValue) {
+            return localStorage.setItem(itemName, JSON.stringify(itemValue))
+        },
+        clear: function clear(itemName) {
+            return localStorage.clear(itemName)
+        }
     }
-  });
 
-  Template.login.events({
-    'submit form': function(e){
-         e.preventDefault();
-        var user = document.getElementById('usernameText').value;
+    function isLoggedIn() {
+        return LocalStorage.getItem('User') ? true : false
+    }
+    Router.route('/', function() {
+        var is = true;
+        console.log(isLoggedIn());
+        if (isLoggedIn()) {
+            this.render('game')
+        } else {
+            this.render('login');
+        }
+    });
 
-          createUser(user);
-      }
+    Template.login.events({
+        'submit form': function(e) {
+            e.preventDefault();
+            var user = document.getElementById('usernameText').value;
 
-  });
-
-
-
-
-
-
-
-
-
+            createUser(user);
+            setTimeout(function() {
+                    if (isLoggedIn()) {
+                        window.location.replace("/");
+                    }
+                },
+                2000);
+        }
+    });
 }
-
-
-
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  })
+    Meteor.startup(function() {
+        // code to run on server at startup
+    })
 }
