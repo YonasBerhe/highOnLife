587a95980c5ea9d59ec0bc7d1e4faf5b879cf148
Yonas Berhe <sanoy@Yonass-MacBook-Air.local>
2015-10-10 20:37:06 -0700

    routing kinda done, need to get the form to create and store user locally

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,8 @@
 // var loginForm = document.getElementByID('loginForm');
 //
-// loginForm.onSubmit() = function(){
+// loginForm.onSubmit() = function(e) {
+//   console.log('main working');
+//     e.preventDefault();
 //     var name = document.getElementById('username').value;
 //     updateUsername(name);
 // }
