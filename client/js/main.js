fb507be505ed958526b06a25535221bae447d156
= <kristoffer@kristofferhebert.com>
2015-10-11 13:05:54 -0700

    fixing the login issue

--- client/js/main.js
+++
@@ -1,8 +0,0 @@
-// var loginForm = document.getElementByID('loginForm');
-//
-// loginForm.onSubmit() = function(e) {
-//   console.log('main working');
-//     e.preventDefault();
-//     var name = document.getElementById('username').value;
-//     updateUsername(name);
-// }
