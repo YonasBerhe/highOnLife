35dbafc7fbcd89ee5a663a07120db350d3f33cc0
bpshonyak <bpshonyak@live.com>
2015-10-10 19:03:14 -0700

    removed unnessisary code

--- client/games/realgame.js
+++ client/games/realgame.js
@@ -21,11 +21,8 @@ function preload() {
 
 }
 
-var ball;
-var paddle;
+var player;
 var drugs;
-
-var ballOnPaddle = true;
 
 var lives = 3;
 var score = 0;
@@ -60,28 +57,15 @@ function create() {
   }
 
 
-  paddle = game.add.sprite(game.world.centerX, game.world.centerY, 'breakout', 'paddle_big.png');
-  paddle.anchor.setTo(0.5, 0.5);
-  paddle.scale.setTo(1, 1);
+  player = game.add.sprite(game.world.centerX, game.world.centerY, 'breakout', 'paddle_big.png');
+  player.anchor.setTo(0.5, 0.5);
+  player.scale.setTo(1, 1);
 
-  game.physics.enable(paddle, Phaser.Physics.ARCADE);
+  game.physics.enable(player, Phaser.Physics.ARCADE);
 
-  paddle.body.collideWorldBounds = true;
-  paddle.body.bounce.set(1);
-  // paddle.body.immovable = true;
-
-  // ball = game.add.sprite(game.world.centerX, paddle.y - 16, 'breakout', 'ball_1.png');
-  // ball.anchor.set(0.5);
-  // ball.checkWorldBounds = true;
-
-  // game.physics.enable(ball, Phaser.Physics.ARCADE);
-  //
-  // ball.body.collideWorldBounds = true;
-  // ball.body.bounce.set(1);
-  //
-  // ball.animations.add('spin', ['ball_1.png', 'ball_2.png', 'ball_3.png', 'ball_4.png', 'ball_5.png'], 50, true, false);
-  //
-  // ball.events.onOutOfBounds.add(ballLost, this);
+  player.body.collideWorldBounds = true;
+  player.body.bounce.set(1);
+  // player.body.immovable = true;
 
   scoreText = game.add.text(32, 550, 'score: 0', {
     font: "20px Arial",
@@ -99,61 +83,29 @@ function create() {
   //   align: "center"
   // });
   // introText.anchor.setTo(0.5, 0.5);
-
-  // game.input.onDown.add(releaseBall, this);
-
 }
 
 function update() {
 
   if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-    paddle.x -= 4;
+    player.x -= 4;
   } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-    paddle.x += 4;
+    player.x += 4;
   }
 
   if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-    paddle.y -= 4;
+    player.y -= 4;
   } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-    paddle.y += 4;
+    player.y += 4;
   }
 
 
-  // if (paddle.body.x == drug.body.x && paddle.body.y == drug.body.y) {
+  // if (player.body.x == drug.body.x && player.body.y == drug.body.y) {
   //   console.log("drugs!");
   // } else {
-  // game.physics.arcade.collide(ball, paddle, ballHitPaddle, null, this);
-  game.physics.arcade.overlap(paddle, drugs, playerHitdrug, null, this);
+  // game.physics.arcade.collide(ball, player, ballHitplayer, null, this);
+  game.physics.arcade.overlap(player, drugs, playerHitdrug, null, this);
   // }
-}
-
-function releaseBall() {
-
-  if (ballOnPaddle) {
-    ballOnPaddle = false;
-    ball.body.velocity.y = -300;
-    ball.body.velocity.x = -75;
-    ball.animations.play('spin');
-    introText.visible = false;
-  }
-
-}
-
-function ballLost() {
-
-  lives--;
-  livesText.text = 'lives: ' + lives;
-
-  if (lives === 0) {
-    gameOver();
-  } else {
-    ballOnPaddle = true;
-
-    ball.reset(paddle.body.x + 16, paddle.y - 16);
-
-    ball.animations.stop();
-  }
-
 }
 
 function gameOver() {
@@ -165,7 +117,7 @@ function gameOver() {
 
 }
 
-function playerHitdrug(_paddle, _drug) {
+function playerHitdrug(_player, _drug) {
   console.log('Collision!');
   _drug.kill();
 
@@ -174,41 +126,14 @@ function playerHitdrug(_paddle, _drug) {
   scoreText.text = 'score: ' + score;
 
   //  Are they any drugs left?
-  if (drugs.countLiving() == 0) {
+  if (drugs.countLiving() === 0) {
     //  New level starts
     score += 1000;
     scoreText.text = 'score: ' + score;
-    introText.text = '- Next Level -';
-
-    //  Let's move the ball back to the paddle
-    // ballOnPaddle = true;
-    // ball.body.velocity.set(0);
-    // ball.x = paddle.x + 16;
-    // ball.y = paddle.y - 16;
-    // ball.animations.stop();
+    // introText.text = '- Next Level -';
 
     //  And bring the drugs back from the dead :)
     drugs.callAll('revive');
-  }
-
-}
-
-function ballHitPaddle(_ball, _paddle) {
-  console.log('Collision!');
-  var diff = 0;
-
-  if (_ball.x < _paddle.x) {
-    //  Ball is on the left-hand side of the paddle
-    diff = _paddle.x - _ball.x;
-    _ball.body.velocity.x = (-10 * diff);
-  } else if (_ball.x > _paddle.x) {
-    //  Ball is on the right-hand side of the paddle
-    diff = _ball.x - _paddle.x;
-    _ball.body.velocity.x = (10 * diff);
-  } else {
-    //  Ball is perfectly in the middle
-    //  Add a little random X to stop it bouncing straight up!
-    _ball.body.velocity.x = 2 + Math.random() * 8;
   }
 
 }
